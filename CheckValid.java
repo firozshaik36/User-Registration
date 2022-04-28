e0f5f35b31af42a9a4b408821c3590d0c62ed489
firozshaik36 <firozshaik0007@gmail.com>
2022-04-28 13:19:41 +0530

    Added:User Need To Enter a Valid First Name.

--- CheckValid.java
+++ CheckValid.java
@@ -4,9 +4,13 @@ import java.util.regex.Pattern;
 
 public class CheckValid {
 
-    public static final String NAME_PATTERN = "^[A-z][a-z]{3,}$";
+    public static final String NAME_PATTERN = "^[A-Z][a-z]{3,}$";
 
     public boolean validFirstName(String name){
+        return Pattern.matches(NAME_PATTERN,name);
+    }
+
+    public boolean validlastName(String name){
         return Pattern.matches(NAME_PATTERN,name);
     }
 }
