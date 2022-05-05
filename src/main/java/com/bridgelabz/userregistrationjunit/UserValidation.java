59928bf3d02188457545737ffbfdcc4ccc387ba1
firozshaik36 <firozshaik0007@gmail.com>
2022-05-05 10:50:48 +0530

    REFACTORED:Java Exception to Solve The Use Cases.

--- src/main/java/com/bridgelabz/userregistrationjunit/UserValidation.java
+++ src/main/java/com/bridgelabz/userregistrationjunit/UserValidation.java
@@ -1,53 +1,67 @@
 package com.bridgelabz.userregistrationjunit;
 
+import com.bridgelabz.userregistrationjunit.UserValidationException.ExceptionType;
+
 public class UserValidation {
 
-    public boolean validateName(String name) {
+    public boolean validateName(String name){
 
-        if(name == null) {
-            return false;
+        try {
+            if (name.length() == 0) {
+                throw new UserValidationException(ExceptionType.ENTERED_EMPTY,"Please Enter Valid Name. EMPTY Name Entered.");
+            }
+            String nameRegex = "^[A-Z]{1}[a-z]{2,}$";
+            return name.matches(nameRegex);
+
         }
-        if(name == "") {
-            return false;
+        catch (NullPointerException e) {
+            throw new UserValidationException(ExceptionType.ENTERED_NULL,"Please Enter Valid Name. NULL Name Entered.");
         }
-        String nameRegex = "^[A-Z]{1}[a-z]{2,}$";
-        return name.matches(nameRegex);
     }
 
-    public boolean validateEmail(String email) {
 
-        if(email == null) {
-            return false;
+    public boolean validateEmail(String email){
+
+        try {
+            if (email.length() == 0) {
+                throw new UserValidationException(ExceptionType.ENTERED_EMPTY,"Please Enter Valid Email Address. EMPTY Email Address Entered.");
+            }
+            String emailRegex = "^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$";
+            return email.matches(emailRegex);
         }
-        if(email == "") {
-            return false;
+        catch (NullPointerException e) {
+            throw new UserValidationException(ExceptionType.ENTERED_NULL,"Please Enter Valid Email Address. NULL Email Address Entered.");
+
         }
-        String emailRegex = "^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$";
-        return email.matches(emailRegex);
     }
 
-    public boolean validateMobileNumber(String mobileNumber) {
+    public boolean validateMobileNumber(String mobileNumber){
 
-        if(mobileNumber == null) {
-            return false;
+        try {
+            if (mobileNumber.length() == 0) {
+                throw new UserValidationException(ExceptionType.ENTERED_EMPTY,"Please Enter Valid Mobile Number. EMPTY Mobile Number Entered.");
+            }
+            String mobileNumberRegex = "^[0-9]{1,2}\\s{1}[0-9]{10}$";
+            return mobileNumber.matches(mobileNumberRegex);
         }
-        if(mobileNumber == "") {
-            return false;
+        catch (NullPointerException e) {
+            throw new UserValidationException(ExceptionType.ENTERED_NULL,"Please Enter Valid Mobile Number. NULL Mobile Number Entered.");
         }
-        String mobileNumberRegex = "^[0-9]{1,2}\\s{1}[0-9]{10}$";
-        return mobileNumber.matches(mobileNumberRegex);
     }
 
     public boolean validatePassword(String password) {
 
-        if(password == null) {
-            return false;
+        try {
+            if (password.length() == 0) {
+                throw new UserValidationException(ExceptionType.ENTERED_EMPTY,"Please Enter Valid Password. EMPTY Password Entered.");
+            }
+            String passwordRegex = "^(?=.*[A-Z])(?=.*[0-9])(?=.{8,}$)[a-zA-Z0-9]*[@#$%^&-+=()][a-zA-Z0-9]*$";
+            return password.matches(passwordRegex);
+
+        } catch (NullPointerException e) {
+            throw new UserValidationException(ExceptionType.ENTERED_NULL,"Please Enter Valid Password. NULL Password Entered.");
         }
-        if(password == "") {
-            return false;
-        }
-        String passwordRegex = "^(?=.*[A-Z])(?=.*[0-9])(?=.{8,}$)[a-zA-Z0-9]*[@#$%^&-+=()][a-zA-Z0-9]*$";
-        return password.matches(passwordRegex);
     }
+
 
 }
---
+++ src/main/java/com/bridgelabz/userregistrationjunit/UserValidationException.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.userregistrationjunit;
+
+public class UserValidationException extends RuntimeException{
+
+    enum ExceptionType {
+        ENTERED_NULL, ENTERED_EMPTY
+    }
+
+    ExceptionType type;
+
+    public UserValidationException(ExceptionType type, String message) {
+
+        super(message);
+        this.type = type;
+
+    }
+}
